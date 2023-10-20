1b39ba0fe446b683242a7c82dd9a6024049a232b
YenlikN <enursafina777@gmail.com>
2023-10-20 13:06:37 +0200

    RecommendationEntity

---
+++ demo/src/main/java/com/example/demo/dl/domain/RecommendationEntity.java
@@ -0,0 +1,12 @@
+package com.example.demo.dl.domain;
+
+import java.time.LocalDate;
+
+public class RecommendationEntity {
+    private Long recommendationId;
+    private String title;
+    private String description;
+    private UserEntity user;
+    private GoalEntity Goal;
+
+}
