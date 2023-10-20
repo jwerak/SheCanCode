02df5698f130bfc797d0f2bb65bf66042850445d
tskheyel <tskheyel@cvut.cz>
2023-10-20 14:01:17 +0200

    recommendation entity

--- demo/src/main/java/com/example/demo/dl/domain/RecommendationEntity.java
+++ demo/src/main/java/com/example/demo/dl/domain/RecommendationEntity.java
@@ -9,4 +9,43 @@ public class RecommendationEntity {
     private UserEntity user;
     private GoalEntity Goal;
 
+    public Long getRecommendationId() {
+        return recommendationId;
+    }
+
+    public void setRecommendationId(Long recommendationId) {
+        this.recommendationId = recommendationId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public GoalEntity getGoal() {
+        return Goal;
+    }
+
+    public void setGoal(GoalEntity goal) {
+        Goal = goal;
+    }
 }
